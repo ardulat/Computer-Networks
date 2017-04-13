03256f2676b7187eea31bb53a22cd808b2b354bc
Sukhrat <sukhrat.arziyev@gmail.com>
2017-04-14 04:15:10 +0600

    Fixed some issues with JTable, adding some hardcoded data

--- NetworksProject1/src/Frame.java
+++ NetworksProject1/src/Frame.java
@@ -12,13 +12,16 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class Frame extends JFrame {
 
 	private JTextField textField;
 	private JButton searchButton;
 	private JButton downloadButton;
-	private List<Object[]> list ;
+	private Object[][] data;
+	private JTable table;
+	DefaultTableModel dtm;
 
 	public Frame() {
 		super("Project 1");
@@ -30,12 +33,21 @@ public class Frame extends JFrame {
 		searchButton = new JButton("Search");
 		add(searchButton);
 
-		Object columnNames[] = { "Name", "Last Modified", "Size" };
-		list =  new ArrayList<Object[]> (1);
-		String[] empty = new String[] {"", "", ""};
-		list.add(empty);
-		Object[][] rowData = (Object[][]) list.toArray();
-		JTable table = new JTable( rowData, columnNames);
+		table = new JTable();
+		dtm = new DefaultTableModel(0, 0);
+
+		// add header of the table
+		String header[] = new String[] { "Name", "Last Modified", "Size" };
+
+		// add header in table model
+		dtm.setColumnIdentifiers(header);
+		// set model into the table object
+		table.setModel(dtm);
+
+		// add row dynamically into the table
+		for (int count = 1; count <= 30; count++) {
+			dtm.addRow(new Object[] { "data", "data", "data" });
+		}
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane, BorderLayout.CENTER);
 
@@ -49,12 +61,17 @@ public class Frame extends JFrame {
 
 	private class ButtonHandler implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
+			
 			if (event.getActionCommand() == "Search") {
-				list.get(0)[0] = "index.html";
-				list.get(0)[1] = "12/05/16";
-				list.get(0)[2] = "5 kb";
+				//clear table, don't need anything to mess there
+				int rowCount = dtm.getRowCount();
+				for (int i = rowCount - 1; i >= 0; i--) {
+					dtm.removeRow(i);
+				}
+				Object[] data = {textField.getText(), "14/02", "15kb"}; 
+				dtm.addRow(data);
 
-			} else {
+			} else if (event.getActionCommand() == "Download") {
 			}
 		}
 	}
